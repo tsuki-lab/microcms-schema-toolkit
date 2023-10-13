52246646c7941296ae492ffa80105aa425301798
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2023-10-13 23:08:20 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# npm-package-starter
+
+Repositories that can be used as a starter in creating npm package.
+
+| dependencies | version |
+| ------------ | ------- |
+| pnpm         |  7.5.0  |
+| typescript   |  4.8.4  |
+
+## Usage
+
+Click "**Use this template**" here or **clone** this repository.
+
+###  Preset commands
+
+### build
+
+Compile `/src/index.ts` as an entry file.
+
+```shell
+pnpm build
+```
---
+++ build.js
@@ -0,0 +1,31 @@
+// @ts-check
+const { build } = require('esbuild')
+const packageJson = require('./package.json')
+
+// @ts-ignore Checked Object keys
+const dependencies = packageJson.hasOwnProperty('dependencies') ? packageJson.dependencies : {}
+const entryFile = 'src/index.ts'
+
+/** @type {import('esbuild').BuildOptions} */
+const shared = {
+  bundle: true,
+  entryPoints: [entryFile],
+  external: Object.keys(dependencies),
+  logLevel: 'info',
+  minify: true,
+  sourcemap: false,
+}
+
+build({
+  ...shared,
+  format: 'esm',
+  outfile: './dist/index.esm.js',
+  target: ['ES6']
+});
+
+build({
+  ...shared,
+  format: 'cjs',
+  outfile: './dist/index.cjs.js',
+  target: ['ES6'],
+})
