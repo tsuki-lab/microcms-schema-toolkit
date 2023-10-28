12ca35a66ddc4dfdfe3568df317e9d8e60234101
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2023-10-28 11:07:06 +0900

    fix: build setting

--- packages/microcms-schema-gen/build.js
+++ packages/microcms-schema-gen/build.js
@@ -1,35 +1,36 @@
 /* eslint-env node */
 
 // @ts-check
-const { build } = require("esbuild");
-const packageJson = require("./package.json");
+const { build } = require('esbuild');
+const packageJson = require('./package.json');
 
 // @ts-ignore Checked Object keys
-const dependencies = Object.hasOwnProperty.call(packageJson, "dependencies")
+const dependencies = Object.hasOwnProperty.call(packageJson, 'dependencies')
   ? packageJson.dependencies
   : {};
-const entryFile = "src/index.ts";
+const entryFile = 'src/index.ts';
 
 /** @type {import('esbuild').BuildOptions} */
 const shared = {
   bundle: true,
   entryPoints: [entryFile],
   external: Object.keys(dependencies),
-  logLevel: "info",
+  logLevel: 'info',
   minify: true,
   sourcemap: false,
+  treeShaking: true,
 };
 
 build({
   ...shared,
-  format: "esm",
-  outfile: "./dist/index.esm.js",
-  target: ["ES6"],
+  format: 'esm',
+  outfile: './dist/index.esm.js',
+  target: ['ES6'],
 });
 
 build({
   ...shared,
-  format: "cjs",
-  outfile: "./dist/index.cjs.js",
-  target: ["ES6"],
+  format: 'cjs',
+  outfile: './dist/index.cjs.js',
+  target: ['ES6'],
 });
